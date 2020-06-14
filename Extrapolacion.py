dff2b9c6604f73595b777d90a08f307fa86a0b0e
Crhxs <66912225+Crhxs@users.noreply.github.com>
2020-06-14 09:39:57 -0500

    Add files via upload

---
+++ Extrapolacion.py
@@ -0,0 +1,26 @@
+print("Bienvenido al proyecto sobre extrapolacion avanzada de Crhistopher")
+a = int(input("Inserte su bin: "))
+fecha = input("Introduce la fecha de su cc T1: ")
+b = int(input("Inserte el primer digito de T1: "))
+c = int(input("Inserte el segundo digito de T1: "))
+d = int(input("Inserte el primer digito de T2: "))
+e = int(input("Inserte el segundo digito de T2: "))
+
+f = b + c
+g = d + e
+
+h = f / 2
+i = g / 2
+
+j = h * 5
+k = i * 5
+
+l = int(j)
+m = int(k)
+
+n = l + m
+
+print("La extrapolación ha sido realizada con éxito: ")
+print(a,n,"xxxxxx")
+print(fecha)
+print("Gen")
